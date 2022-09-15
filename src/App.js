d1f757cd098ea9299c889ed03e26a24767702fe4
William Rosander <william.rosander@lysa.se>
2022-09-15 17:35:59 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-logo" alt="logo">
+          Solceller är bajs, det är fakta
+        </div>
+
+        <p>Vill du vet mer så fråga Marthin</p>
       </header>
     </div>
   );
